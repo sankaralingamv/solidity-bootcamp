af4cc9fbe17de17bf38de27d075330491a5cda7a
sankaralingam <122398798+sankaralingamv@users.noreply.github.com>
2024-02-05 19:39:14 +0530

    Update homework3.sol

--- risein/homework3.sol
+++ risein/homework3.sol
@@ -1,1 +1,25 @@
+pragma solidity ^0.8.18;
 
+contract ProposalContract {
+
+   uint256 private counter;
+
+   struct Proposal {
+       string title;
+       string description;
+       uint256 approve;
+       uint256 reject;
+       uint256 pass;
+       uint256 total_vote_to_end;
+       bool current_state;
+       bool is_active;
+   }
+
+   mapping(uint256 => Proposal) public proposal_history;
+
+   function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external {
+       counter += 1;
+       uint256 id = counter;
+       proposal_history[id] = Proposal(_title, _description, 0, 0, 0, _total_vote_to_end, false, true);
+   }
+}
